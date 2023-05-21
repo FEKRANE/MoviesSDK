44debaf9f96fc42414557280f0cde931fa02e50b
FEKRANE <zakariafekrane@gmail.com>
2023-05-21 13:59:38 +0100

    Initial Commit

---
+++ MoviesSDK/MoviesSDK.h
@@ -0,0 +1,18 @@
+//
+//  MoviesSDK.h
+//  MoviesSDK
+//
+//  Created by ADRIA on 21/5/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MoviesSDK.
+FOUNDATION_EXPORT double MoviesSDKVersionNumber;
+
+//! Project version string for MoviesSDK.
+FOUNDATION_EXPORT const unsigned char MoviesSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoviesSDK/PublicHeader.h>
+
+
