19d6b2bed9c7d3ab697c5b286c774b429c8083af
FEKRANE <zakariafekrane@gmail.com>
2023-05-21 21:27:11 +0100

    [ADD] Add constant struct to group all SDK values

---
+++ MoviesSDK/Common/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  MoviesSDK
+//
+//  Created by FEKRANE on 21/5/2023.
+//
+
+import Foundation
+
+struct Constants {
+   
+    struct API {
+        static let upcomingReleases = "https://moviesdatabase.p.rapidapi.com/titles/x/upcoming"
+    }
+       
+}
