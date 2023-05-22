d02cad6b385aa9922d95e53593d6528a56880554
FEKRANE <zakariafekrane@gmail.com>
2023-05-22 11:03:28 +0100

    [ADD] UpcomingReleases model to be exposed for clients

---
+++ MoviesSDK/Models/MovieInfo.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  MoviesSDK
+//
+//  Created by FEKRANE on 22/5/2023.
+//
+
+import Foundation
+
+public struct MovieInfo {
+    public let title: String
+    public let imageUrl: String
+    public let releaseDate: Date
+}
---
+++ MoviesSDK/Models/UpcomingReleases.swift
@@ -0,0 +1,13 @@
+//
+//  UpcomingReleases.swift
+//  MoviesSDK
+//
+//  Created by FEKRANE on 22/5/2023.
+//
+
+import Foundation
+
+public struct UpcomingReleases {    
+    public let count: Int
+    public let movies: [MovieInfo]
+}
